ec355e5ca14e327bbeca11ff129c026d0a61358e
naki <nakiagca10@gmail.com>
2023-07-06 20:08:37 +0300

    AppUserRegisterDto class ++

--- EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
+++ EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos
 {
-    internal class AppUserRegisterDto
+    public class AppUserRegisterDto
     {
+        //böyle attributeler eklenip yapılır ama biz böyle yapmayacağız.
+        //[Required(ErrorMessage = "Ad alanı zorunludur")]
+        //[Display(Name = "İsim")]
+        //[MaxLength(30,ErrorMessage = "Enfazla 30 karakter girebilirsiniz")]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
+
+    //ad,soyad,username,email,şifre,confirmpassword lazım burda bize
 }
