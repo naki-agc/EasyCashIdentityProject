71b052a475b10cb3aaf47c5727b02bccb1399ff6
naki <nakiagca10@gmail.com>
2023-07-21 12:17:07 +0300

    ConfirmMailController, MyProfileController and Index added

--- EasyCashIdentityProject.PresantationLayer/Models/ConfirmMailViewModel.cs
+++ EasyCashIdentityProject.PresantationLayer/Models/ConfirmMailViewModel.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace EasyCashIdentityProject.PresentationLayer.Models
+﻿namespace EasyCashIdentityProject.PresentationLayer.Models
 {
 	public class ConfirmMailViewModel
     {
-        public int Id { get; set; }
+        public string Mail { get; set; }
         public int ConfirmCode { get; set; }
     }
 }
